a3ea4848f477e02aeb06b79436c1fa05a02196a9
JordyC21 <87671558+JordyC21@users.noreply.github.com>
2021-09-13 16:40:42 -0600

    Update DatosProductos.cs

--- Parcial/Datos/DatosProductos.cs
+++ Parcial/Datos/DatosProductos.cs
@@ -5,9 +5,7 @@ namespace Datos
 {
     public class DatosProductos
     {
-        //Estos objetos simularan nuestra base de datos
-        //Tenemos una tiendita con 5 productos y
-        //cada producto tiene su id, nombre, precio, marca y existencias
+
         EntidadProducto producto1 = new EntidadProducto(1, "Café instantáneo", "", 500, 60);
         EntidadProducto producto2 = new EntidadProducto(2, "Café instantáneo", "", 500, 60);
         EntidadProducto producto3 = new EntidadProducto(3, "Café instantáneo", "", 500, 60);
@@ -16,9 +14,7 @@ namespace Datos
 
         public EntidadProducto buscarProductoPorID(EntidadProducto producto) {
 
-            //Este metodo nos devolverá los datos completos del producto
-            //Para saber que producto devolver debemos obtener el id del producto
-            //Dependiendo de ese id, así devolveremos el producto correspondiente
+
             if (producto.Id == 1)
             {
                 producto = producto1;
@@ -51,11 +47,9 @@ namespace Datos
             }
         }
 
-        //Para vender productos, solo necesitamos el producto a vender
-        //asi como la cantidaque venderemos
+
         public EntidadProducto venderProducto(EntidadProducto producto, int cantidad) {
-            //Para simular la venta, solo restaremos la cantidad vendida a las existencias
-            //de nuestro producto
+ 
             producto.Cobro = (producto.Preciodelproducto * cantidad).ToString("00.00");
 
             return producto;
